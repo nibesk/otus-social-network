e443f03a4f81c218a0e0294c257077b9794b96f1
nikitabesx <nik.besx@gmail.com>
2020-08-09 16:40:28 +0300

    service log fix

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/badThug/otus-social-network/app"
 	"github.com/badThug/otus-social-network/app/config"
+	"log"
 )
 
 func main() {
+	log.Println("Server has been started")
+
 	config := config.InitConfig()
 	app := app.Init(config)
 	app.Run()
